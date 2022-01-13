6d1524bd6ecd2a492f359d8e2ffdd71cacdb16ec
Jesse Varis <jessevaris@hotmail.com>
2022-01-13 11:44:37 +0100

    Infinit loop...

--- home.js
+++ home.js
@@ -1,15 +1,29 @@
 let number = 1;
 
-while(number <= 100) {
-    if( (number % 3) === 0 )  {
-        if( number % 4 === 0 )  {
-        console.log('Bish-Bosh');
-    }
-    console.log('Bish');
-}
-    else if ( number % 4 === 0 )  {
-    console.log('Bosh');
-}
-number++;
-console.log(number);
-}
+// while(number <= 100) {
+//     if( (number % 3) === 0 )  {
+//         if( (number % 4) === 0 )  {
+//         console.log('Bish-Bosh');
+//     }
+//     console.log('Bish');
+// }
+//     else if ( (number % 4) === 0 )  {
+//     console.log('Bosh');
+// }
+// number++;
+// }
+
+// while(number <= 100 )  {
+//     if( ((number % 3) === 0) || ((number % 4) === 0 ))  {
+//         console.log('Bish-Bosh');
+//     }
+//     else if ((number % 3) === 0)  {
+//         console.log('Bish');
+//     }
+//     else if ((number % 4) === 0)  {
+//         console.log('Bosh');
+//     }
+//         else console.log(number);
+//     }
+//     console.log(number)
+// number++;
