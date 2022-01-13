78cbfdf65778d6aeb71d072d89ddeaca49ad4605
Jesse Varis <jessevaris@hotmail.com>
2022-01-13 12:53:46 +0100

    Save

--- home.js
+++ home.js
@@ -3,16 +3,15 @@ let number = 1;
 while(number <= 100 )  {
     if( ((number % 3) === 0) && ((number % 4) === 0 ))  {
         console.log('Bish-Bosh');
-        // number++;
     }
     else if ((number % 3) === 0)  {
         console.log('Bish');
-        // number++;
     }
     else if ((number % 4) === 0)  {
         console.log('Bosh');
-        // number++;
     }
-        else console.log(number);
-        number++;
+    else  {
+        console.log(number);
     }
+    number++;
+}
